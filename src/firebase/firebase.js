d158eefa4ea0c9f86d71e5abdd9ed0ce573842bc
Andrea <andrea.bolognesi@gmail.com>
2017-11-20 12:59:01 +0100

    Fix on setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAchbzyIGxYEKDZzgDRU3GtaCt9QmuGYao",
-    authDomain: "expensify-c7fb4.firebaseapp.com",
-    databaseURL: "https://expensify-c7fb4.firebaseio.com",
-    projectId: "expensify-c7fb4",
-    storageBucket: "expensify-c7fb4.appspot.com",
-    messagingSenderId: "500073895424"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
